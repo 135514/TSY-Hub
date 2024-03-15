f759e527dfa48243e7acbe1b3edc55126fb230b3
135514 <109018168+135514@users.noreply.github.com>
2024-03-16 02:33:34 +0800

    Update test.c
    添加排序

--- test.c
+++ test.c
@@ -2,14 +2,42 @@
 #define Syringe_count  3
 #define Dosage_count  5
 #define injection_maxnum 20
-int main()
-{
-    
-    /*三种注射器 1 3 5*/
+/*三种注射器 1 3 5*/
     int Syringe[Syringe_count] = {1, 3, 5};
     /*五种药剂容量*/
     double Dosage[Dosage_count] = {0.8, 2.0, 3.0, 4.0, 5.0};
 
+void Syringe_sort(int *Syringe, int size)  //数据排序方便后续寻找最佳注射器
+{  
+    int i, j, tmp;  
+    for (i = 0; i < size - 1; i++) {  
+        for (j = 0; j < size - i - 1; j++) {  
+            if (Syringe[j] > Syringe[j+1]) {  
+                tmp = Syringe[j];  
+                Syringe[j] = Syringe[j+1];  
+                Syringe[j+1] = tmp;  
+            }  
+        }  
+    }  
+} 
+void Dosage_sort(double *Dosage, int size)     //数据排序方便后续寻找最佳注射器
+{  
+    int i, j, tmp;  
+    for (i = 0; i < size - 1; i++) {  
+        for (j = 0; j < size - i - 1; j++) {  
+            if (Dosage[j] > Dosage[j+1]) {  
+                tmp = Dosage[j];  
+                Dosage[j] = Dosage[j+1];  
+                Dosage[j+1] = tmp;  
+            }  
+        }  
+    }  
+} 
+int main()
+{
+    Syringe_sort(Syringe, Syringe_count);
+    Dosage_sort(Dosage,Dosage_count);
+    
     for (int i = 0; i < Dosage_count; i++)         //药剂找注射器，找到了就退出
     {
   
@@ -27,6 +55,6 @@ int main()
 
     }
         
-        
-    }
+    return 0;    
+}
     
